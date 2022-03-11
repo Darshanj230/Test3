7a25fc523164fd1572ec292fe2a842eb91e2b119
Darshanj230 <100772063+Darshanj230@users.noreply.github.com>
2022-03-11 23:35:38 +0530

    Update HelloWorld.java

--- src/test3/HelloWorld.java
+++ src/test3/HelloWorld.java
@@ -3,8 +3,8 @@ package test3;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Syste.out.println("Hello World");
+            
 	}
 
 }
